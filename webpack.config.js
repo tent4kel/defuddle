839228f73d6e70f7455ac38aef1f39b8ff593738
Steph Ango <10565871+kepano@users.noreply.github.com>
2025-02-28 09:21:21 -0800

    Add dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,45 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    library: {
-      name: 'Defuddle',
-      type: 'umd'
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    mode: argv.mode || 'production',
+    entry: './src/index.ts',
+    devtool: isDevelopment ? 'inline-source-map' : false,
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+      library: {
+        name: 'Defuddle',
+        type: 'umd'
+      },
+      globalObject: 'typeof self !== "undefined" ? self : this'
     },
-    globalObject: 'typeof self !== "undefined" ? self : this'
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              configFile: 'tsconfig.json'
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                configFile: 'tsconfig.json'
+              }
             }
-          }
-        ],
-        exclude: /node_modules/
-      }
-    ]
-  }
+          ],
+          exclude: /node_modules/
+        }
+      ]
+    },
+    optimization: {
+      // Ensure consistent output in both dev and prod
+      moduleIds: 'deterministic',
+      // Disable eval
+      minimize: !isDevelopment
+    }
+  };
 }; 
